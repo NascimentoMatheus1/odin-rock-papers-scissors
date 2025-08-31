a3accbbfd444f88e4631af0d82aec82c54d7f37d
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2025-08-31 17:45:59 -0300

    adicionado UI

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-const form = document.querySelector('form');
-const playerInput = document.getElementById('playerInput');
-const rounds = document.getElementById('rounds');
 const result = document.getElementById('gameWinner');
 const playerPoints = document.getElementById('playerScore');
 const computerPoints = document.getElementById('computerScore');
 
+const btnRock = document.getElementById('btnRock');
+const btnPaper = document.getElementById('btnPaper');
+const btnScissors = document.getElementById('btnScissors');
+
+const playerMove = document.getElementById('playerImage');
+const computerMove = document.getElementById('computerImage');
+
 let playerScore = 0;
 let ComputerScore = 0;
-let numRouds = 0;
 
-form.addEventListener("submit", (e)=> {
+btnRock.addEventListener('click', (e) => {
     e.preventDefault();
-    playGame()
-})
+    playRound('rock');
+});
+
+btnPaper.addEventListener('click', (e) => {
+    e.preventDefault();
+    playRound('paper');
+});
+
+btnScissors.addEventListener('click', (e) => {
+    e.preventDefault();
+    playRound('scissors');
+});
 
 function getComputerChoice(){
     const Choices = ["rock", "paper", "scissors"];
@@ -20,29 +33,22 @@ function getComputerChoice(){
     return Choices[randommNum];
 }
 
-function getHumanChoice(){
-    const hChoice = playerInput.value ;
-    return hChoice;
-}
+function playRound(playerChoice){
 
-function playRound(){
-    const playerChoice = getHumanChoice();
     const computerChoice = getComputerChoice();
-    playerInput.value = '';
-    numRouds++
 
     if(isPlayerWon(playerChoice, computerChoice) == 'draw'){
-        const drawMessage = `${playerChoice} VS ${computerChoice} IT'S A DRAW !!!`;
+        const drawMessage = `IT'S A DRAW !!!`;
         updateBoard(drawMessage, playerChoice, computerChoice)
 
     }else if(isPlayerWon(playerChoice, computerChoice)){
         ++playerScore;
-        const playerWonMessage = `${playerChoice} beats ${computerChoice} ! PLAYER WON !!!`;
+        const playerWonMessage = `PLAYER WON !!!`;
         updateBoard(playerWonMessage, playerChoice, computerChoice);
         
     }else {
         ++ComputerScore;
-        const computerWonMessage = `${computerChoice} beats ${playerChoice} ! COMPUTER WON !!!`;
+        const computerWonMessage = `COMPUTER WON !!!`;
         updateBoard(computerWonMessage, playerChoice, computerChoice);
     }
 }
@@ -64,40 +70,14 @@ function isPlayerWon(playerChoice, computerChoice){
     return result;
 }
 
-function playGame(){
-    if(numRouds < 5){
-        playRound()
-    }else{
-        let winner = '';
-        if(playerScore ==  ComputerScore){
-            winner = 'DRAW !!!';
-        }else if (playerScore > ComputerScore){
-            winner = 'PLAYER WON !!!';
-        }else{
-            winner = 'COMPUTER WON !!!';
-        }
-        alert(winner);
-
-        const aswer = confirm('NEW GAME ?')
-        if(aswer){
-
-            playerScore = 0;
-            ComputerScore = 0;
-            numRouds = 0;
-            updateBoard('', '', '');
-            playGame()
-        }
-    }
-}
 
 function updateBoard(message, playerChoice, computerChoice){
-    rounds.innerText = ''
-    rounds.innerText = `ROUND ${numRouds} ||| Player move: ${playerChoice} |||-||| Computer move: ${computerChoice}`
+    playerMove.innerHTML = `<img src="images/${playerChoice}.png" alt="${playerChoice} hand gesture">`;
+    computerMove.innerHTML = `<img src="images/${computerChoice}.png" alt="${computerChoice} hand gesture">`
 
     result.innerText = '';
     result.innerText = message;
 
-    
-    playerPoints.innerText = `Player points: ${playerScore}`;
-    computerPoints.innerText = `Computer points: ${ComputerScore}`;
+    playerPoints.innerText = ` : ${playerScore}`;
+    computerPoints.innerText = ` : ${ComputerScore}`;
 }
